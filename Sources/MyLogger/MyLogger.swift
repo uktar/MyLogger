7152cf1e307d0a054a58765422d16e9e4994bd5b
uktar <uktar@cht.com.tw>
2022-05-05 11:23:25 +0800

    fix leak

--- Sources/MyLogger/MyLogger.swift
+++ Sources/MyLogger/MyLogger.swift
@@ -200,24 +200,10 @@ extension OutputStream {
     
     func write(_ string: String, encoding: String.Encoding = String.Encoding.utf8, allowLossyConversion: Bool = true) -> Int {
         if let data = string.data(using: encoding, allowLossyConversion: allowLossyConversion) {
-            var bytes = (data as NSData).bytes.bindMemory(to: UInt8.self, capacity: data.count)
-            var bytesRemaining = data.count
-            var totalBytesWritten = 0
-            
-            while bytesRemaining > 0 {
-                let bytesWritten = self.write(bytes, maxLength: bytesRemaining)
-                if bytesWritten < 0 {
-                    return -1
-                }
-                
-                bytesRemaining -= bytesWritten
-                bytes += bytesWritten
-                totalBytesWritten += bytesWritten
+            return data.withUnsafeBytes {
+                self.write($0.baseAddress!.assumingMemoryBound(to: UInt8.self), maxLength: data.count)
             }
-            
-            return totalBytesWritten
         }
-        
         return -1
     }
     
